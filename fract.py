c6af3ff74ed637af76192d6f72afc9adc09e2051
ACastello <a.castello.pas@gmail.com>
2017-05-30 11:31:12 +0200

    .

--- fract.py
+++ fract.py
@@ -6,18 +6,85 @@ This is a temporary script file.
 """
 
 from PIL import Image
+import PIL
 from scipy import log,log2,floor,average
-# from multiprocessing import Pool
+from multiprocessing import Pool
 import matplotlib.pyplot as plt
 
-BASE=2.0
-
-def logb(base, exp):
+class box:
     """
-    Implementacion del logaritmo en base n
+    Clase contenedora de variables importantes. Tambien ayuda a generar 
+    recursivamente los datos necesarios para cualquier operación
     """
-    return log(exp)/log(base)
+    def __init__(self, filepath, threshold=128):
+        self.filepath = filepath
+        self.threshold = threshold
+        self.img = im = Image.open(filepath).convert('L')
+        self.w, self.h = im.size
+        self.min = min(self.w, self.h)
+        self.max = max(self.w, self.h)
+        self.pixmap = im.load()
+        self.coefs = None
+        self.sets = None
+        self.dims = None
+        self.m = None
+        self.n = None
+            
+    def coeficientes(self, g=0, f = lambda x: x):
+        factors = [ 2 * i + 1 for i in range(2 ** g) ]
+        pots = lambda e: [e * (2 ** i) for i in range( int(log2(self.min/e)))]
+        sides = reduce(lambda x, y: x + pots(y), factors, [])
+        sides.sort()
+        sides = f(sides)
+        self.coefs = sides
+        return sides
 
+    def recubrimientos(self, g=0):
+        if self.coefs == None:
+            self.coeficientes(g)
+        L = {k: set() for k in self.coefs}
+        
+        for i in range(self.w):
+            for j in range(self.h):
+                if self.pixmap[i,j] < self.threshold:
+                    for e,s in L.items():
+                        s.add( (i//e, j//e) )
+        
+        self.sets = L
+        return L
+
+    def dimensiones(self, g=0, plot=True):
+        if self.sets == None:
+            self.recubrimientos(g)
+        L = []
+        offset = log2(self.max)
+        for k,v in self.sets.items():
+            L.append( ( -log2(k)+offset, log2(len(v)) ) )
+        L.sort()
+        self.dims = L
+        self.m, self.n = regresion(L)
+        if plot:
+            self.plot()
+        return L
+        
+    def plot(self):
+        x = map(lambda (x,_): x, self.dims)
+        y = map(lambda (_,y): y, self.dims)
+        def f(x):
+            return self.m * x + self.n
+        plt.rc('grid', linestyle=":", color='black', alpha=0.5)
+        plt.rc('font', **{'sans-serif' : 'Arial',
+                           'family' : 'sans-serif'})
+        plt.figure(1)
+        plt.subplot(111)
+        plt.xlabel(u"-log δ")
+        plt.ylabel(u"log N(δ)")
+        plt.grid()
+        plt.plot(x, y, 'ro')
+        plt.plot([x[0]-0.25, x[-1]+0.25],[f(x[0]-0.25), f(x[-1]+0.25)],'b-')
+        plt.show()
+        
+        
 def pixeles(path):
     """
     :param path: localizacion de la imagen a cargar (todos los formatos soportados)
@@ -29,22 +96,58 @@ def pixeles(path):
     # L = [[pix[i,j] for j in range(h)] for i in range(w)]
     return (w, h, pix)
 
-def coeficientes(w,h,granularity=0):
-    """
-    :param w: altura de la imagen
-    :param h: ancho de la imagen
-    :param granularity: granularidad, orden de magnitud de numeros primos a 
-        usar como base aparte del 1
-    Devuelve una lista de valores que indican el ancho de las distintas cajas 
-    a usar
-    """
-    dim = min(h,w)
-    factors = [ 2 * i + 1 for i in range(2**granularity) ]
-    
-    sides = map(lambda x: [x * (2 ** i) for i in range( int(log2(dim/x) + 1) )], factors)    
-    
-    return sides
+def dimensiones(sets):
+    L = []
+    offset = log2(sets['dim'])
+    for k,v in sets.items():
+        if type(k) == int:
+            L.append((-log2(k)+offset, log2(len(v))))
+    return sorted(L)
 
+def regresion(L, N = None):
+    """
+    Toma o bien una lista de tuplas `(x,y)` o dos listas de numeros `[x]`, `[y]`
+    y devuelve los coeficientes para formar la recta de regresion de los datos
+    """
+    if N == None:
+        N = map(lambda (_,y): y, L)
+        L = map(lambda (x,_): x, L)
+    l = min(len(L), len(N))
+    avgx = average(L)
+    avgy = average(N)
+    cov = sum([ L[i]*N[i] for i in range(l) ]) / l - avgx*avgy
+    varx = sum([ (L[i] - avgx)**2 for i in range(l) ]) / l 
+    m = cov/varx
+    n = avgy - (cov/varx) * avgx
+    # print(avgx,avgy,cov,varx)
+    # la recta de regresión será: Y = m*X + n
+    return (m,n)    
+
+def plotdims(dims):
+    x = map(lambda (x,_): x, dims)
+    y = map(lambda (_,y): y, dims)
+    m,n = regresion(x,y)
+    def f(x):
+        return m*x + n
+    plt.rc('grid', linestyle=":", color='black', alpha=0.5)
+    plt.figure(1)
+    plt.subplot(111)
+    plt.xlabel(u'-log δ')
+    plt.ylabel(u'log N(δ)')
+    plt.grid()
+    plt.plot(x, y, 'ro')
+    plt.plot([x[0]-0.25, x[-1]+0.25],[f(x[0]-0.25), f(x[-1]+0.25)],'b-')
+    plt.show()
+    
+def final(fp, g=0):
+    w,h,pix = pixeles(fp)
+    coefs = coeficientes(w,h,g)
+    recs = recubrimientos(w,h,coefs,pix)
+    dims = dimensiones(recs)
+    plotdims(dims)
+    m, n = regresion(dims)
+    return m
+    
 def boxcofs(L, plot=False, ndrop=0):
     meds = [(BASE**(-1-i)) for i in range(len(L))][::-1]
     x = map(lambda x: -log(x), meds)[ndrop:]
@@ -104,20 +207,4 @@ def f(path, ndrop=1, plot=True):
     # print cofs
     # return stats(map(lambda x: -log(x), cofs), map(log, L)) 
 
-def stats(L, N):
-    l = min(len(L), len(N))
-    avgx = average(L)
-    avgy = average(N)
-    cov = sum([ L[i]*N[i] for i in range(l) ]) / l - avgx*avgy
-    varx = sum([ (L[i] - avgx)**2 for i in range(l) ]) / l 
-    m = cov/varx
-    n = avgy - (cov/varx) * avgx
-    # print(avgx,avgy,cov,varx)
-    return (m,n)    
-    # la recta de regresión será: Y = m*X + n
-    
-
-
-    # buenos = map(lambda e: filter(lambda x: x < 255, e), l)
-    # return sum(map(len, buenos))
 
