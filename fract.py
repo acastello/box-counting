9d22a056126d5c881049cf2ae32f1faaa96553b1
Alejandro Castello <a.castello.pas@gmail.com>
2017-06-08 11:46:49 +0000

    .

--- fract.py
+++ fract.py
@@ -5,15 +5,80 @@ Spyder Editor
 This is a temporary script file.
 """
 
-from PIL import Image
-from scipy import log,log2,floor,average
-from multiprocessing import Pool
+from PIL import Image, ImageColor
+from scipy import log2,average
+import os
 import matplotlib.pyplot as plt
 
-class box:
-    """
-    Clase contenedora de variables importantes. Tambien ayuda a generar 
-    recursivamente los datos necesarios para cualquier operación
+class Box:
+    u"""
+    Clase principal para generar datos intermedios y finales, los intermedios
+    se generan automática y recursivamente, quedan guardados de modo que se 
+    pueden llamar las funciones finales repetidamente, y si los parámetros no 
+    cambian, no se recalculan datos innecesariamente
+
+    :param filepath: nombre de la imagen a abrir
+    :param umbral: (opcional) valor del color de un pixel por debajo del cual 
+                   considerarlo como opaco, por defecto es 85
+    
+    
+    - La función :func:`dimension` devuelve la dimensión box-counting calculada
+      según la recta de regresión y, si no se especifca lo contrario, muestra un 
+      grafico para visualizar la distribucion de la dimensión respecto a los 
+      distintos recubrimientos.
+    
+    - :func:`imagen` genera una imagen que representa un recubrimiento concreto 
+      de la imagen original
+    
+    Ejemplo::
+        
+        # nos vale el umbral estándar asique no lo especificamos
+        b = Box("sierpinski.png")
+        
+        b.dimension(plot=False)
+        # el resultado no nos convence, queremos que nos muestre el grafico
+        
+        b.dimension()
+        # tras llamar `dimension`, se nos muestra un grafico 
+        # de la distribucion de la dimensión box-counting, y no es muy lineal
+
+        # queremos ignorar el último dato (el correspondiente al recubrimiento
+        # más fino) y los 2 primeros
+        b.dimension(pre=2, post=1)
+        
+        # podemos utilizar recubrimientos cuadrados de la longitud (en pixeles)
+        # que queramos
+        b.dimension(g=[1,3,9,27])
+        
+        # tambien podemos generar exponencialmente mas coeficientes dandole 
+        # valores enteros a la `g`
+        # g = 0 -> [1,2,4,8, ..]
+        # g = 1 -> [1,2,3,4,6,8,12, ..]
+        # g = 2 -> [1,2,3,4,5,6,7,8,10,12,14,16,20, ..]
+        
+        # tambien podemos excluir los puntos que vemos en el grafico dandole 
+        # un valor decimal a `pre` o `post`
+        
+        # vemos que los valores con -log(𝛿) menores que 3.0 no nos valen
+        b.dimension(pre=3.0)
+        
+        # generamos una imagen que ilustre el recubrimiento con lado 17 
+        b.imagen(17)
+        
+        # generamos la misma imagen, pero sin malla, y sin superponerla sobre
+        # la imagen original
+        b.imagen(17, imposed=False, grid=False)
+        
+        # generamos la misma imagen pero especificando rojo como color para las
+        # celdas que recubren y negro para la malla
+        b.imagen(17, color=(255,0,0), linecolor="black")
+        
+        # también se puede variar la opacidad de las celdas
+        b.imagen(17, alpha=200)
+                    
+        # en todos estos casos se ha generado y guardado la imagen 
+        # sierpinski_17.png
+        
     """
     
     coef = 1    
@@ -21,14 +86,14 @@ class box:
     dime = 3
     
     
-    def __init__(self, filepath, threshold=128):
+    def __init__(self, filepath, umbral=85):
         self.filepath = filepath
-        self.threshold = threshold
-        self.img = im = Image.open(filepath).convert('L')
+        self.umbral = umbral
+        self.img = im = Image.open(filepath)
         self.w, self.h = im.size
         self.min = min(self.w, self.h)
         self.max = max(self.w, self.h)
-        self.pixmap = im.load()
+        self.pixmap = im.convert('L').load()
         self.stage = 0
         self.g = 0
             
@@ -37,44 +102,57 @@ class box:
             self.coefs = self.g = g
             return g
         factors = [ 2 * i + 1 for i in range(2 ** g) ]
-        pots = lambda e: [e * (2 ** i) for i in range( int(log2(self.min/e)))]
+        pots = lambda e: [e * (2 ** i) for i in range( int(log2(self.min/e)) )]
         sides = reduce(lambda x, y: x + pots(y), factors, [])
         sides.sort()
         sides = f(sides)
         self.coefs = sides
         self.g = g
-        self.stage = box.coef
+        self.stage = Box.coef
         return sides
 
     def recubrimientos(self, g=0):
-        if self.stage < box.recu or g != self.g:
+        if self.stage >= Box.recu and g == self.g and self.coefs == self.sets.keys:
+            return self.sets
+        elif self.stage < Box.recu:
             self.coeficientes(g)
-        L = {k: set() for k in self.coefs}
+            self.sets = {}
+        keys = self.sets.keys()
+        L = {k: set() for k in self.coefs if not k in keys}
         
         for i in range(self.w):
             for j in range(self.h):
-                if self.pixmap[i,j] < self.threshold:
+                if self.pixmap[i,j] < self.umbral:
                     for e,s in L.items():
                         s.add( (i//e, j//e) )
         
         self.sets = L
-        self.stage = box.recu
+        self.stage = Box.recu
         return L
 
-    def dimensiones(self, g=0, plot=True, pre=0, post=0):
-        if self.stage < box.dime or g != self.g:
+    def dimension(self, g=0, plot=True, pre=0, post=0):
+        if self.stage < Box.dime or g != self.g:
             self.recubrimientos(g)
         L = []
         offset = log2(self.max)
         for k,v in self.sets.items():
             L.append( ( -log2(k)+offset, log2(len(v)) ) )
         L.sort()
-        L = L[pre:len(L)-post]
+        if type(pre) == int:
+            L = L[pre:]
+        elif type(pre) == float:
+            L = [(i,j) for (i,j) in L if i >= pre]
+            
+        if type(post) == int:
+            L = L[:len(L)-post]
+        elif type(post) == float:
+            L = [(i,j) for (i,j) in L if i <= post]
+
         self.dims = L
         self.m, self.n = regresion(L)
         if plot:
             self.plot()
-        self.stage = box.dime
+        self.stage = Box.dime
         return L,self.m
         
     def plot(self):
@@ -90,37 +168,80 @@ class box:
         plt.ylabel(r'log N($\delta$)')
         plt.grid()
         plt.plot(x, y, 'ro')
-        plt.plot([x[0]-0.25, x[-1]+0.25],[f(x[0]-0.25), f(x[-1]+0.25)],'b-')
+        plt.plot([x[0]-0.5, x[-1]+0.5],[f(x[0]-0.5), f(x[-1]+0.5)],'b-')
         plt.show()
+    
+    def imagen(self, escala, imposed=True, grid=True, color=0xfffffd7b, 
+               linecolor=0xffff4436, alpha=127, **opts):
+        self.recubrimientos(**opts)
+        if self.sets.has_key(escala):
+            points = self.sets[escala]
+        else:
+            self.coefs.append(escala)
+            self.recubrimientos()
+            points = self.sets[escala]
         
+        if type(color) == str:
+            color = ImageColor.getcolor(color, 'RGBA')
+            
+        if type(linecolor) == str:
+            linecolor = ImageColor.getcolor(linecolor, 'RGBA')
         
-def pixeles(path):
-    """
-    :param path: localizacion de la imagen a cargar (todos los formatos soportados)
-    Devuelve en una tupla: ancho, alto y mapa de pixeles (accesible con [i,j]) de la imagen
-    """
-    im = Image.open(path).convert('L')
-    (w,h) = im.size
-    pix = im.load()
-    # L = [[pix[i,j] for j in range(h)] for i in range(w)]
-    return (w, h, pix)
+        Im = Image.new('RGBA', (self.w, self.h), (255,255,255,0))
+        pix = Im.load()
+        
+        Mask = Image.new('L', (self.w, self.h), 0)
+        maskpix = Mask.load()
+        for (x,y) in points:
+            for i in range(escala):
+                for j in range(escala):
+                    if escala * x + i < self.w and escala * y + j < self.h:
+                        pix[escala * x + i, escala * y + j] = color
+                        maskpix[escala * x + i, escala * y + j] = alpha
+        if grid:
+            Grid = Image.new('RGBA', (self.w, self.h), 0x0)
+            gridpix = Grid.load()
+            for (x,y) in points:
+                for i in range(escala+1):
+                    xi = escala * x + i
+                    x0 = escala * x
+                    x1 = escala * (x + 1)
+                    yi = escala * y + i
+                    y0 = escala * y
+                    y1 = escala * (y + 1)
+                    if xi < self.w and y0 < self.h:
+                        gridpix[xi, y0] = linecolor
+                    if xi < self.w and y1 < self.h:
+                        gridpix[xi, y1] = linecolor
+                    if x0 < self.w and yi < self.h:
+                        gridpix[x0, yi] = linecolor
+                    if x1 < self.w and yi < self.h:
+                        gridpix[x1, yi] = linecolor
 
-def dimensiones(sets):
-    L = []
-    offset = log2(sets['dim'])
-    for k,v in sets.items():
-        if type(k) == int:
-            L.append((-log2(k)+offset, log2(len(v))))
-    return sorted(L)
-
+        (base,_) = os.path.splitext(self.filepath)
+        filename = base + '_' + str(escala) + '.png'
+        
+        if imposed:
+            Imf = Image.composite(Im, self.img.convert('RGBA'), Mask)
+        else:
+            Imf = Im
+        
+        if grid:
+            Imfinal = Image.composite(Grid, Imf, Grid)
+        else:
+            Imfinal = Imf
+        
+        Imfinal.save(filename)
+        return filename
+        
 def regresion(L, N = None):
     """
-    Toma o bien una lista de tuplas `(x,y)` o dos listas de numeros `[x]`, `[y]`
-    y devuelve los coeficientes para formar la recta de regresion de los datos
+    Toma o bien una lista de tuplas `(x,y)` o dos listas de numeros `[x]`,
+    `[y]` y devuelve los coeficientes para formar la recta de regresion de
+    los datos
     """
     if N == None:
-        N = map(lambda (_,y): y, L)
-        L = map(lambda (x,_): x, L)
+        [L, N] = zip(*L)
     l = min(len(L), len(N))
     avgx = average(L)
     avgy = average(N)
@@ -131,90 +252,4 @@ def regresion(L, N = None):
     # print(avgx,avgy,cov,varx)
     # la recta de regresión será: Y = m*X + n
     return (m,n)    
-
-def plotdims(dims):
-    x = map(lambda (x,_): x, dims)
-    y = map(lambda (_,y): y, dims)
-    m,n = regresion(x,y)
-    def f(x):
-        return m*x + n
-    plt.rc('grid', linestyle=":", color='black', alpha=0.5)
-    plt.figure(1)
-    plt.subplot(111)
-    plt.xlabel(u'-log δ')
-    plt.ylabel(u'log N(δ)')
-    plt.grid()
-    plt.plot(x, y, 'ro')
-    plt.plot([x[0]-0.25, x[-1]+0.25],[f(x[0]-0.25), f(x[-1]+0.25)],'b-')
-    plt.show()
-    
-def final(fp, g=0):
-    w,h,pix = pixeles(fp)
-    coefs = coeficientes(w,h,g)
-    recs = recubrimientos(w,h,coefs,pix)
-    dims = dimensiones(recs)
-    plotdims(dims)
-    m, n = regresion(dims)
-    return m
-    
-def boxcofs(L, plot=False, ndrop=0):
-    meds = [(BASE**(-1-i)) for i in range(len(L))][::-1]
-    x = map(lambda x: -log(x), meds)[ndrop:]
-    y = map(log, L)[ndrop:]
-    m,n = stats(x,y)
-    def f(x):
-        return m*x + n
-    L2 = [y[i] / x[i] for i in range(len(x))]
-    print zip(L2,L)
-        
-    if plot:
-        plt.rc('grid', linestyle=":", color='black', alpha=0.5)
-        plt.figure(1)
-        plt.subplot(111)
-        plt.xlabel(u'-log δ')
-        plt.ylabel(u'log N(δ)')
-        plt.grid()
-        plt.plot(x, y, 'ro')
-        plt.plot([x[0]+0.25, x[-1]-0.25],[f(x[0]+0.25), f(x[-1]-0.25)],'b-')
-        plt.show()
-    return m
-
-def lcosta(L):
-    cofs = [(2**(-1-i)) for i in range(len(L))][::-1]
-    L2 = []
-    for i in range(len(L)):
-        L2.append(L[i]*cofs[i])
-    return L2
-
-def recuadros(path):
-    (w, h, pix) = pixeles(path)
-    n = int( floor(logb(BASE,max(w,h))) )
-    L = [set() for _ in range(n)]
-    maxx = maxy = 0
-    minx = w-1
-    miny = h-1
-                
-    for i in range(w):
-        for j in range(h):
-            if pix[i,j] < 128:
-                if i < minx: minx = i
-                if i > maxx: maxx = i
-                if j < miny: miny = j
-                if j > maxy: maxy = j
-                # L[0].add((x,y))
-                for k in range(n):
-                    tup = (i // (BASE ** k), j // (BASE ** k))
-                    L[k].add(tup)
-    # factor = max(w,h) / float( max(maxx-minx, maxy-miny) )
-    # print(minx,maxx,miny,maxy)
-    # print factor
-    return L
-            
-def f(path, ndrop=1, plot=True):
-    L = recuadros(path)
-    return boxcofs(map(len,L), plot, ndrop=ndrop)
-    # print cofs
-    # return stats(map(lambda x: -log(x), cofs), map(log, L)) 
-    
-# if __name__ == '__main__':
     
